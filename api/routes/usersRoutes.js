811d824f4468409e5f6cff85b55361d9f97a3847
Liliana Faustino <it@buildtoo.com>
2019-07-30 20:11:40 +0100

    Get all users function

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -2,5 +2,6 @@ const controller = require('../controllers/usersController');
 
 module.exports = (router) => {
     router.route('/users')
+        .get(controller.getAllUsers)
         .post(controller.add);
 };
