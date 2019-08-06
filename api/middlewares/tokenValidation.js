c00f94a253333b96f19707072467e5aacb87219d
Liliana Faustino <it@buildtoo.com>
2019-08-06 15:22:19 +0100

    Changed validation position

--- api/middlewares/tokenValidation.js
+++ api/middlewares/tokenValidation.js
@@ -3,25 +3,24 @@ const jwt = require('jsonwebtoken');
 function validateToken(req, res, next) {
     const authorizationHeader = req.headers.authorization;
     let result;
-    if (authorizationHeader) {
-        const token = req.headers.authorization.split(' ')[1]; // Help
-        const options = { expiresIn: '1d' };
-
-        try {
-            result = jwt.verify(token, process.env.JWT_SECRET, options); // verify if token hasn't expired
-            req.decoded = result;
-            next();
-        }
-        catch (err) {
-            throw new Error(err);
-        }
-    }
-    else {
+    if (!authorizationHeader) {
         result = {
             error: 'Authentication error. Token required.',
             status: 401
         };
         res.status(401).send(result);
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+    const options = { expiresIn: '1d' };
+
+    try {
+        result = jwt.verify(token, process.env.JWT_SECRET, options); // verify if token hasn't expired
+        req.decoded = result;
+        next();
+    }
+    catch (err) {
+        throw new Error(err);
     }
 }
 
